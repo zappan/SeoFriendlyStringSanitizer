c338f28bf4380928dbddd8f92d2ac059894cabd1
Tomislav Capan <tomislav@netko.hr>
2011-09-03 17:12:19 +0200

    NuSpec data and assembly build data

---
+++ Netko.Common.Util.Seo/Netko.Common.Util.Seo.SeoFriendlyStringSanitizer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SEO Friendly String Sanitizer")]
+[assembly: AssemblyDescription("Small library that performs the string conversion into a sanitized version that can be included in the URL")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Netko")]
+[assembly: AssemblyProduct("Netko.Common.Util.Seo.SeoFriendlyStringSanitizer.Properties")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d06def07-1f1f-441e-a815-b97a6ad6a45e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
